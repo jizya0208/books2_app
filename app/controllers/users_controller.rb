bf7831a471e32b33546fc12c5499ca3889629acf
EC2 Default User <ec2-user@ip-172-31-53-32.ec2.internal>
2021-05-15 00:14:02 +0000

    [ADD] User編集ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,8 @@ class UsersController < ApplicationController
   
   def show
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   resources :books, only: [:index, :create, :show, :destroy]
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :users, only: [:create, :show, :edit, :update, :index]
 end
