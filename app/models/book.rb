0a5f607267ac06ad74525ede374ddf54233f6261
EC2 Default User <ec2-user@ip-172-31-53-32.ec2.internal>
2021-05-16 04:46:14 +0000

    [ADD] バリデーション字数制限

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,9 @@
 class Book < ApplicationRecord
+  
   with_options presence: true do
     validates :title
     validates :body
   end
+  validates :body, length: {maximum: 200}
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :name, presence: true, length: {minimum: 2}
+  validates :name, presence: true, length: {minimum: 2, maximum: 20}, uniqueness: true
+  validates :introduction, length: {maximum: 50}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
